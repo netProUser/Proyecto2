d36a12732d4c4d882a6fa2f98521d324df19b2a2
netProUser <guillelr3026@gmail.com>
2024-07-14 22:48:27 -0500

    vw
    vw

--- ProyectoInicial/app/src/main/java/com/example/proyectoinicial/MainViewModel.java
+++ ProyectoInicial/app/src/main/java/com/example/proyectoinicial/MainViewModel.java
@@ -1,42 +1,63 @@
 package com.example.proyectoinicial;
 
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
 public class MainViewModel extends ViewModel {
 
-    private Integer flat1 = 0;
-    private Integer flat2 = 0;
-    private String desc_flat1 = "";
-    private String desc_flat2 = "";
+    //private Integer flat1 = 0;
+    //private Integer flat2 = 0;
+    //private String desc_flat1 = "";
+    //private String desc_flat2 = "";
 
-    public Integer getFlat1() {
+    private final MutableLiveData<Integer>  flat1 = new  MutableLiveData<>();
+    private final MutableLiveData<Integer>  flat2 = new  MutableLiveData<>();
+    private final MutableLiveData<String>  desc_flat1 = new  MutableLiveData<>();
+    private final MutableLiveData<String>  desc_flat2 = new  MutableLiveData<>();
+    private final MutableLiveData<Integer>  flat3 = new  MutableLiveData<>();
+
+    public MainViewModel() {
+        //ResetDatos();
+    }
+
+    void ResetDatos() {
+        flat1.setValue(0);
+        flat2.setValue(0);
+        flat3.setValue(0);
+        desc_flat1.setValue("");
+        desc_flat2.setValue("");
+    }
+    public LiveData<Integer> getFlat1() {
         return flat1;
     }
 
-    public Integer getFlat2() {
+    public LiveData<Integer> getFlat2() {
         return flat2;
     }
 
-    public String getDesc_flat1() {
+    public LiveData<String> getDesc_flat1() {
         return desc_flat1;
     }
 
-    public String getDesc_flat2() {
+    public LiveData<String> getDesc_flat2() {
         return desc_flat2;
     }
 
-    void ValidarValores(String v_usuario, String v_clave) {
-         flat1 = 0;
-         flat2 = 0;
-        if(v_usuario.length() < 8){
-         flat1 = 1;
-         desc_flat1 = "El usuario tiene que ser mayor o igual de 8 caracteres";
-     }
-     if(v_clave.length() < 10){
-         flat2 = 1;
-         desc_flat2 = "La clave tiene que ser mayor o igual de 10 caracteres";
-     }
-
+    public LiveData<Integer> getFlat3() {
+        return flat3;
     }
 
+    void ValidarValores(String v_usuario, String v_clave) {
+        //ResetDatos();
+        if(v_usuario.length() < 8){
+         flat1.setValue(1);
+         desc_flat1.setValue("El usuario tiene que ser mayor o igual de 8 caracteres") ;
+        } else if (v_clave.length() < 10){
+         flat2.setValue(1);
+         desc_flat2.setValue("La clave tiene que ser mayor o igual de 10 caracteres");
+        }else{
+            flat3.setValue(1);
+        }
+    }
 }
--- ProyectoInicial/app/src/main/java/com/example/proyectoinicial/Pagina1.java
+++ ProyectoInicial/app/src/main/java/com/example/proyectoinicial/Pagina1.java
@@ -5,18 +5,18 @@ import android.os.Bundle;
 import android.util.Log;
 import android.widget.Toast;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.example.proyectoinicial.databinding.ActivityPagina1Binding;
 
+import java.util.concurrent.atomic.AtomicReference;
+
 public class Pagina1 extends AppCompatActivity {
 
     private MainViewModel viewModel;
+    String v_usuario = "";
+    String v_clave = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,21 +27,48 @@ public class Pagina1 extends AppCompatActivity {
         viewModel = new ViewModelProvider(this).get(MainViewModel.class);
 
         binding.btnIngresar2.setOnClickListener(v -> {
-            String v_usuario = binding.txtUsuario.getText().toString();
-            String v_clave = binding.txtClave.getText().toString();
+            v_usuario = binding.txtUsuario.getText().toString();
+            v_clave = binding.txtClave.getText().toString();
 
             //Toast.makeText(this,getString(R.string.mensajeAdvertencia),Toast.LENGTH_LONG).show();
             viewModel.ValidarValores(v_usuario,v_clave);
 
-            if(viewModel.getFlat1() == 1){
-                Toast.makeText(this,viewModel.getDesc_flat1(),Toast.LENGTH_LONG).show();
-            } else if (viewModel.getFlat2() == 1) {
-                Toast.makeText(this,viewModel.getDesc_flat2(),Toast.LENGTH_LONG).show();
-            }else {
-                openPrimeraPagina(v_usuario,v_clave);
-            }
+//            viewModel.getFlat1().observe(this, flat1 -> {
+//                //viewModel.getFlat1().setValue(flat1);
+//            });
+
+//            if(viewModel.getFlat1() == 1){
+//                Toast.makeText(this,viewModel.getDesc_flat1(),Toast.LENGTH_LONG).show();
+//            } else if (viewModel.getFlat2() == 1) {
+//                Toast.makeText(this,viewModel.getDesc_flat2(),Toast.LENGTH_LONG).show();
+//            }else {
+//                openPrimeraPagina(v_usuario,v_clave);
+//            }
+
+//            if(flat1 == 1){
+//                Toast.makeText(this,viewModel.getDesc_flat1(),Toast.LENGTH_LONG).show();
+//            } else if (viewModel.getFlat2() == 1) {
+//                Toast.makeText(this,viewModel.getDesc_flat2(),Toast.LENGTH_LONG).show();
+//            }else {
+//                openPrimeraPagina(v_usuario,v_clave);
+//            }
 
         });
+
+        viewModel.getDesc_flat1().observe(this, Desc_flat1 -> {
+            Toast.makeText(this,Desc_flat1,Toast.LENGTH_LONG).show();
+        });
+
+        viewModel.getDesc_flat2().observe(this, Desc_flat2 -> {
+            Toast.makeText(this,Desc_flat2,Toast.LENGTH_LONG).show();
+        });
+
+        viewModel.getFlat3().observe(this, flat3 -> {
+            if(flat3 == 1){
+                openPrimeraPagina(v_usuario,v_clave);
+            }
+        });
+
         Log.d("MainActivity","Activity Created");
     }
     private void openPrimeraPagina(String v_usuario,String v_clave) {
